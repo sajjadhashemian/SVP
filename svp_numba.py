b7a8372f8deff06abcd221e13bb1ebaf45f8aca5
Sajjad Hashemian <shashemianm@gmail.com>
2024-11-15 00:32:13 +0330

    Segmentation fault on n=55 and C=0.3

--- svp_numba.py
+++ svp_numba.py
@@ -14,7 +14,7 @@ from lattice_generator import generate_random_instance, generate_hard_instance, 
 np.random.seed(1337)
 FPLLL.set_random_seed(1337)
 
-n, b= 40, 32
+n, b= 55, 32
 X = generate_random_instance(b, n)
 
 # n, p, r= 17, 97, 3
@@ -27,17 +27,19 @@ SVP.shortest_vector(A)
 s, l = A[0], norm(A[0])
 print('------------------------------------')
 print('fpylll solution:')
-print([int(x) for x in s],'\n Norm:', l)
+# print([int(x) for x in s],'\n Norm:', l)
 # print([int(x) for x in inv(B)@s])
+print('Norm:', l)
 
 print('------------------------------------')
 print('my solution:')
-C = 0.5															#Exponent constant: number of samples, Working fine, but C~20: 2*e*pi
+C = 0.3														#Exponent constant: number of samples, Working fine, but C~20: 2*e*pi
 t1=time.time()
 s, _l = __decision_svp(B.astype(float), n, l, l, C)
 t2=time.time()
 
-print([-int(x) for x in s],'\n Norm:', _l)
+# print([-int(x) for x in s],'\n Norm:', _l)
+print('Norm:', _l)
 
 t=int(t2-t1)
 print('elapsed time: ',t//60, 'm', t%60, 's')
@@ -50,6 +52,7 @@ print('verify solution:')
 
 v0 = CVP.closest_vector(X, tuple([int(x) for x in s]))
 # print('Verify with CVP:      ',[int(x) for x in v0],'\n norm:',norm(v0))
-print('Verdict:', bool(abs(norm(v0)-_l)<1e-3), ', Comparing solutions:', bool(l<=_l+1e-6))
+e=0.0001
+print('Verdict:', bool(abs(norm(v0)-_l)<e) and bool(_l<=l+e), ', ', _l/l)
 
 
